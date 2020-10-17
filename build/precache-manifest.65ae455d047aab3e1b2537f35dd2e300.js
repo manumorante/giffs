f04d1fecd85f67ac2555f20581f7be0350249f41
Manu <manu@estadologico.com>
2020-10-17 19:48:18 +0200

    Build

---
+++ build/precache-manifest.65ae455d047aab3e1b2537f35dd2e300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc39f6d0ef4b1136c63a2d8d758a76c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5bc10042e39d3daff8f9",
+    "url": "./static/css/main.ebebff4f.chunk.css"
+  },
+  {
+    "revision": "b3905cf19f6874979ff6",
+    "url": "./static/js/2.4e5101e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.4e5101e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bc10042e39d3daff8f9",
+    "url": "./static/js/main.8618b411.chunk.js"
+  },
+  {
+    "revision": "5596e5a9fceb6003a5a5",
+    "url": "./static/js/runtime-main.ea4fc9aa.js"
+  }
+]);
