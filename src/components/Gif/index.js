506acd5d471d9f335c096bfd869ab25db298d848
Manu <manu@estadologico.com>
2020-10-18 13:56:04 +0200

    Link detail

--- src/components/Gif/index.js
+++ src/components/Gif/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'wouter'
 import './styles.css'
 
 export default function Gif ({ title, id, url}) {
   return (
     <div className='gif'>
-      <img className='gif__img' key={id} src={url} alt={title} />
-      <div className='gif__title'>{title}</div>
+      <Link to={`/gif/${id}`}>
+        <img className='gif__img' key={id} src={url} alt={title} />
+        <div className='gif__title'>{title}</div>
+      </Link>
     </div>
   )
 }
