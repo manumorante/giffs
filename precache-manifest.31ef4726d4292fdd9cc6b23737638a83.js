8876b1e73380727f5a52d51b2d7c9fc5ce9747b5
Manu Morante <manu@estadologico.com>
2020-10-22 19:55:44 +0200

    Updates

---
+++ precache-manifest.31ef4726d4292fdd9cc6b23737638a83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957781a466a3f4a6db94ecb735dd3222",
+    "url": "/giffs/index.html"
+  },
+  {
+    "revision": "3253c6806b48a721c4d6",
+    "url": "/giffs/static/css/main.ccd1b36d.chunk.css"
+  },
+  {
+    "revision": "093b0e8b92a89b5316c0",
+    "url": "/giffs/static/js/2.732b9d93.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/giffs/static/js/2.732b9d93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3253c6806b48a721c4d6",
+    "url": "/giffs/static/js/main.96e91967.chunk.js"
+  },
+  {
+    "revision": "c76136626b11eb3abd7a",
+    "url": "/giffs/static/js/runtime-main.c9824aba.js"
+  }
+]);
